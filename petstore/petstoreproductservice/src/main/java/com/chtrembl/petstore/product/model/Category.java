ae61880ce9c1e5568c7f2f6579d56a02d8861ad3
Mateusz <mateki@o2.pl>
2023-08-28 17:55:41 +0200

    update table names

--- petstore/petstoreproductservice/src/main/java/com/chtrembl/petstore/product/model/Category.java
+++ petstore/petstoreproductservice/src/main/java/com/chtrembl/petstore/product/model/Category.java
@@ -19,7 +19,7 @@ import javax.persistence.Table;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-12-20T15:31:39.272-05:00")
 
 @Entity
-@Table(name = "category")
+@Table(name = "CATEGORY")
 public class Category {
 	@JsonProperty("id")
 	@Id
--- petstore/petstoreproductservice/src/main/java/com/chtrembl/petstore/product/model/Product.java
+++ petstore/petstoreproductservice/src/main/java/com/chtrembl/petstore/product/model/Product.java
@@ -23,7 +23,7 @@ import io.swagger.annotations.ApiModelProperty;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2021-12-20T15:31:39.272-05:00")
 
 @Entity
-@Table(name = "product")
+@Table(name = "PRODUCT")
 public class Product {
 	@JsonProperty("id")
 	@Id
