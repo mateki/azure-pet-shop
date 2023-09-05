5ace17fbec9e8e95e45b40a3acc2619c911589f9
Mateusz <mateki@o2.pl>
2023-09-05 20:56:43 +0200

    retry order reservation

---
+++ petstore/ordersreservation/src/main/java/com/chtrembl/petstore/pet/BusReservation.java
@@ -0,0 +1,108 @@
+package com.chtrembl.petstore.pet;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Optional;
+
+/**
+ * Azure Functions with Service Bus Trigger.
+ */
+public class BusReservation {
+    /**
+     * This function will be invoked when a new message is received at the Service Bus Queue.
+     */
+    @FunctionName("orderServiceBusReservation")
+    public void run(
+            @ServiceBusQueueTrigger(name = "message", queueName = "reservation", connection = "ServiceBusConnection") String message,
+            @BindingName("SessionId") String sessionId,
+            final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Service Bus Queue trigger function executed.");
+        context.getLogger().info(message);
+
+        HttpStatus response = dbReservation(context, message,sessionId);
+        for (int i = 0; i < 3; i++) {
+            if (HttpStatus.OK == response) {
+                return;
+            } else {
+                response = dbReservation(context,  message,sessionId);
+            }
+        }
+        reservationFallback(context, message);
+    }
+
+    private HttpStatus dbReservation(final ExecutionContext context, String message, String sessionId) {
+        context.getLogger().info("DB save attempt");
+
+        // Get the name parameter from the request
+
+
+        // Get the Blob Storage connection string and container name from environment variables
+        String storageConnectionString = System.getenv("AzureWebJobsStorage");
+        String containerName = "reservations";
+
+        try {
+            // Create a CloudBlobClient object using the connection string
+            CloudBlobClient blobClient = CloudStorageAccount.parse(storageConnectionString).createCloudBlobClient();
+
+            // Get a reference to the container
+            CloudBlobContainer container = blobClient.getContainerReference(containerName);
+
+            // Create the container if it does not exist
+            container.createIfNotExists();
+
+            // Create a new blob with a random name
+            String blobName = (StringUtils.isBlank(sessionId) ? java.util.UUID.randomUUID().toString() : sessionId) + ".json";
+            CloudBlockBlob blob = container.getBlockBlobReference(blobName);
+
+            // Upload the name parameter to the blob
+            blob.uploadText(message);
+
+
+        } catch (Exception e) {
+            context.getLogger().info("DB save failure");
+            return HttpStatus.FAILED_DEPENDENCY;
+        }
+        return HttpStatus.OK;
+    }
+
+
+    private void reservationFallback(final ExecutionContext context, String message) {
+        try {
+            // Get the name parameter from the request
+
+            String mailerUrl = System.getenv("MailSender");
+            context.getLogger().info("calling fallback mail sender "+mailerUrl);
+            URL url = new URL(mailerUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            OutputStream os = connection.getOutputStream();
+            os.write(message.getBytes());
+            os.flush();
+            os.close();
+
+            String responseMessage = connection.getResponseMessage();
+            if(connection.getResponseCode()==200){
+                context.getLogger().info("fallback ok");
+            }{
+                context.getLogger().info("fallback failure "+responseMessage);
+            }
+
+        } catch (Exception e) {
+            context.getLogger().info("fallback failure "+e);
+        }
+
+    }
+}
---
+++ petstore/petstoreorderservice/src/main/java/com/chtrembl/petstore/order/api/ServiceBusOrderRepository.java
@@ -0,0 +1,124 @@
+package com.chtrembl.petstore.order.api;
+
+import com.azure.cosmos.models.CosmosItemRequestOptions;
+import com.azure.cosmos.models.CosmosItemResponse;
+import com.azure.cosmos.models.PartitionKey;
+import com.azure.messaging.servicebus.*;
+import com.chtrembl.petstore.order.model.Order;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@Primary
+@Repository
+public class ServiceBusOrderRepository implements ItemReservationRepository{
+    static final Logger log = LoggerFactory.getLogger(ServiceBusOrderRepository.class);
+
+    @Value("${petstore.service.reservations.bus.url:Endpoint=sb://mdjava-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=k0CT/uBUMzSdQ3QwFH/TXF6D3ewlwa2Xn+ASbMgr5pA=}")
+    private String connectionString;
+
+    @Value("${petstore.service.reservations.bus.ques:reservation}")
+    private String queueName;
+
+
+    @Override
+    public void put(Order order) {
+        ServiceBusSenderClient senderClient = new ServiceBusClientBuilder()
+                .connectionString(connectionString)
+                .sender()
+                .queueName(queueName)
+                .buildClient();
+
+        // send one message to the queue
+        String oerder = null;
+        try {
+            oerder = new ObjectMapper().writeValueAsString(order);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        ServiceBusMessage serviceBusMessage = new ServiceBusMessage(oerder);
+        serviceBusMessage.setSessionId(order.getId());
+        senderClient.sendMessage(serviceBusMessage);
+        System.out.println("Sent a single message to the queue: " + queueName);
+    }
+
+    @Override
+    public Optional<Order> get(String id) {
+        return Optional.empty();
+    }
+
+
+    // handles received messages
+    public void receiveMessages() throws InterruptedException
+    {
+        CountDownLatch countdownLatch = new CountDownLatch(1);
+
+        // Create an instance of the processor through the ServiceBusClientBuilder
+        ServiceBusProcessorClient processorClient = new ServiceBusClientBuilder()
+                .connectionString(connectionString)
+                .processor()
+                .queueName(queueName)
+                .processMessage(this::processMessage)
+                .processError(context -> processError(context, countdownLatch))
+                .buildProcessorClient();
+
+        System.out.println("Starting the processor");
+        processorClient.start();
+
+        TimeUnit.SECONDS.sleep(10);
+        System.out.println("Stopping and closing the processor");
+        processorClient.close();
+    }
+    private static void processError(ServiceBusErrorContext context, CountDownLatch countdownLatch) {
+        System.out.printf("Error when receiving messages from namespace: '%s'. Entity: '%s'%n",
+                context.getFullyQualifiedNamespace(), context.getEntityPath());
+
+        if (!(context.getException() instanceof ServiceBusException)) {
+            System.out.printf("Non-ServiceBusException occurred: %s%n", context.getException());
+            return;
+        }
+
+        ServiceBusException exception = (ServiceBusException) context.getException();
+        ServiceBusFailureReason reason = exception.getReason();
+
+        if (reason == ServiceBusFailureReason.MESSAGING_ENTITY_DISABLED
+                || reason == ServiceBusFailureReason.MESSAGING_ENTITY_NOT_FOUND
+                || reason == ServiceBusFailureReason.UNAUTHORIZED) {
+            System.out.printf("An unrecoverable error occurred. Stopping processing with reason %s: %s%n",
+                    reason, exception.getMessage());
+
+            countdownLatch.countDown();
+        } else if (reason == ServiceBusFailureReason.MESSAGE_LOCK_LOST) {
+            System.out.printf("Message lock lost for message: %s%n", context.getException());
+        } else if (reason == ServiceBusFailureReason.SERVICE_BUSY) {
+            try {
+                // Choosing an arbitrary amount of time to wait until trying again.
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                System.err.println("Unable to sleep for period of time");
+            }
+        } else {
+            System.out.printf("Error source %s, reason %s, message: %s%n", context.getErrorSource(),
+                    reason, context.getException());
+        }
+    }
+
+
+    private void processMessage(ServiceBusReceivedMessageContext context) {
+        ServiceBusReceivedMessage message = context.getMessage();
+        System.out.printf("Processing message. Session: %s, Sequence #: %s. Contents: %s%n", message.getMessageId(),
+                message.getSequenceNumber(), message.getBody());
+    }
+
+
+}
